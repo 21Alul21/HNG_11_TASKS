cbbd4298d5210c1cdaa0088d60ff4d71e3613a7c
21Alul21 <augustine.alul@yahoo.com>
2024-07-06 09:52:29 +0100

    added blueprint

--- UserAuthenticationOrganization/app.py
+++ UserAuthenticationOrganization/app.py
@@ -1,144 +1,44 @@
 from flask import Flask, jsonify, request
-from flask_jwt_extended import jwt_manager, jwt_required, create_access_token, get_jwt_identity
+from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identity
 from models import db, User, Organization
 import bcrypt
+from auth_blueprint import JWT, auth
+from dotenv import load_dotenv
 
 
+load_dotenv()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb'
+app.config['JWT_SECRETE_KEY'] = 'SFEFEFEVEVEVEVEV'
+
+app.register_blueprint(auth)
 
 db.init_app(app)
+JWT.init_app(app)
+     
+@app.route('api/users/<id>', methods=['GET'], strict_slashes=None)
+@jwt_required
+def user_record(id):
+      
+      """ view that returns the user's record in organizations
+        they belong to or created 
+      """
 
 
-@app.route('/auth/register', methods=['POST'], strict_slashes=False)
-def register_user():
-    """ view for handling user registeration """
-    user_info = request.get_json
-   
-    if user_info:
-        firstName = user_info.get('firstName')
-        lastName = user_info.get('lastName')
-        email = user_info.get('email')
-        password = user_info.get('password')
-        phone = user_info.get('phone')
 
 
-        #validating input fields
 
-        # firstname field validation
-        if firstName is None:
-            return jsonify({
-                'errors': [
-                    {'field': 'firstName',
-                     'message': 'First name field cannot be blank'
-                     
-                     }
 
-                ]                
-            }), 422
-        
-        if len(firstName) >= 20:
-            return jsonify({
-                'errors': [
-                    {'field': 'firstName',
-                     'message': 'First name is too long, it shoulbe less than 20'
-                     }
-                ]     
-            }), 422
-        
-
-        # last name field validation
-        if lastName is None:
-            return jsonify({
-                'errors': [
-                    {'field': 'lastName',
-                     'message': 'last name field cannot be blank'
-                     }
-                ]                
-            }), 422
-        
-        if len(lastName) >= 20:
-            return jsonify({
-                'errors': [
-                    {'field': 'lasttName',
-                     'message': 'last name is too long, it shoulbe less than 20'
-                     
-                     }
-                ]     
-            }), 422
-        
-        # email field validation
-        if email is None:
-            return jsonify({
-                'errors': [
-                    {'field': 'email',
-                     'message': 'email field cannot be blank'
-                     }
-                ]                
-            }), 422
-        
-        if '@' and '.' not in email:
-                  return jsonify({
-                'errors': [
-                    {'field': 'email',
-                     'message': 'invalid email address'
-                     }
-                ]                
-            }), 422
-        
-        # password field validation
-        if not password:
-                   return jsonify({
-                'errors': [
-                    {'field': 'password',
-                     'message': 'password field cannot be blank'
-                     }
-                ]                
-            }), 422
-        
-        if not password.isalphanumeric():
-             return jsonify({
-                'errors': [
-                    {'field': 'password',
-                     'message': 'password field must contain both numbers and characters'
-                     }
-                ]                
-            }), 422
-        
-        if len(password) < 4:
-             return jsonify({
-                'errors': [
-                    {'field': 'password',
-                     'message': 'password is too short'
-                     }
-                ]                
-            }), 422
-        
-        # phone number field validation
-        if not phone:
-             return jsonify({
-                'errors': [
-                    {'field': 'phone',
-                     'message': 'phone field must contain both numbers and characters'
-                     }
-                ]                
-            }), 422
-                  
-        if phone.isalphanumeric():
-             return jsonify({
-                'errors': [
-                    {'field': 'phone',
-                     'message': 'invalid input, phone field must not contain alphabets'
-                     }
-                ]                
-            }), 422
-                  
-        
               
 
         
-        pw_salt = bcrypt.gensalt()
-        hashed_pw = bcrypt.hashpw(password.encode('utf-8'), pw_salt)
+
+
+
+
+        
+
+     
 
 
         
---
+++ UserAuthenticationOrganization/auth_blueprint.py
@@ -0,0 +1,262 @@
+from flask import Flask, jsonify, request, Blueprint
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
+from models import db, User, Organization
+import bcrypt
+from models import db
+
+auth = Blueprint(name='authentication', url_prefix='auth', import_name=__file__)
+
+JWT = JWTManager()
+
+
+@auth.route('/register', methods=['POST'], strict_slashes=False)
+def register_user():
+    """ view for handling user registeration """
+    user_info = request.get_json
+   
+    if user_info:
+        firstName = user_info.get('firstName')
+        lastName = user_info.get('lastName')
+        email = user_info.get('email')
+        password = user_info.get('password')
+        phone = user_info.get('phone')
+
+
+        #validating input fields
+
+        # firstname field validation
+        if firstName is None:
+            return jsonify({
+                'errors': [
+                    {'field': 'firstName',
+                     'message': 'First name field cannot be blank'
+                     }
+                ]                
+            }), 422
+        
+        if len(firstName) >= 20:
+            return jsonify({
+                'errors': [
+                    {'field': 'firstName',
+                     'message': 'First name is too long, it shoulbe less than 20'
+                     }
+                ]     
+            }), 422
+        
+
+        # last name field validation
+        if lastName is None:
+            return jsonify({
+                'errors': [
+                    {'field': 'lastName',
+                     'message': 'last name field cannot be blank'
+                     }
+                ]                
+            }), 422
+        
+        if len(lastName) >= 20:
+            return jsonify({
+                'errors': [
+                    {'field': 'lasttName',
+                     'message': 'last name is too long, it shoulbe less than 20'
+                     }
+                ]     
+            }), 422
+        
+        # email field validation
+        if email is None:
+            return jsonify({
+                'errors': [
+                    {'field': 'email',
+                     'message': 'email field cannot be blank'
+                     }
+                ]                
+            }), 422
+        
+        if '@' and '.' not in email:
+                  return jsonify({
+                'errors': [
+                    {'field': 'email',
+                     'message': 'invalid email address'
+                     }
+                ]                
+            }), 422
+        
+        existing_email = User.querry.filter_by(email=email).first()
+        if existing_email:
+             return jsonify({
+                  'errors': [
+                    {'field': 'email',
+                     'message': 'email address already exist'
+                     }
+                  ]
+             }), 422
+
+        
+        # password field validation
+        if not password:
+                   return jsonify({
+                'errors': [
+                    {'field': 'password',
+                     'message': 'password field cannot be blank'
+                     }
+                ]                
+            }), 422
+        
+        if not password.isalphanumeric():
+             return jsonify({
+                'errors': [
+                    {'field': 'password',
+                     'message': 'password field must contain both numbers and characters'
+                     }
+                ]                
+            }), 422
+        
+        if len(password) < 4:
+             return jsonify({
+                'errors': [
+                    {'field': 'password',
+                     'message': 'password is too short'
+                     }
+                ]                
+            }), 422
+        
+        # phone number field validation
+        if not phone:
+             return jsonify({
+                'errors': [
+                    {'field': 'phone',
+                     'message': 'phone field must contain both numbers and characters'
+                     }
+                ]                
+            }), 422
+                  
+        if phone.isalphanumeric():
+             return jsonify({
+                'errors': [
+                    {'field': 'phone',
+                     'message': 'invalid input, phone field must not contain alphabets'
+                     }
+                ]                
+            }), 422
+        
+        # hashing the user's password
+        pw_salt = bcrypt.gensalt()
+        hashed_pw = bcrypt.hashpw(password.encode('utf-8'), pw_salt)
+        user = User(firstName=firstName, lastName=lastName, email=email, password=hashed_pw, phone=phone)
+        db.session.add(user)
+        db.session.commit()
+        access_token = create_access_token(identity=user.userId)
+
+        # creating default organization register
+        name = firstName + 's' + ' Organization'
+        organization = Organization(orgId=user.userId, name=name, description='')
+        db.session.add(organization)
+        db.session.commit()
+
+        return jsonify({
+            'status': 'success',
+            'message': 'Registration successful',
+            'data': {
+                  'accessToken': access_token,
+                  'user': {
+                        'userId': user.userId,
+                        'firstName': user.firstName,
+                        'lastName': user.lastName,
+                        'email': user.email,
+                        'phone': user.phone
+                   }
+            }
+        }), 201
+    
+    return jsonify({
+          'status': 'Bad request',
+          'message': 'Registration unsuccessful',
+          'statusCode': 400
+    }), 400
+
+
+@auth.route('/login', methods=['POST'], strict_slashes=False)
+@jwt_required
+def login_user():
+     """ view function that handles user login """
+
+     user_info = request.get_json
+     if user_info:
+        email = user_info.get('email')
+        password = user_info.get('password')
+
+        # email validation
+        if not email:
+             return jsonify({
+                'errors': [
+                    {'field': 'email',
+                     'message': 'email field cannot be blank'
+                     }
+                ]                
+            }), 422
+        
+        if '@' and '.' not in email:
+                  return jsonify({
+                'errors': [
+                    {'field': 'email',
+                     'message': 'invalid email address'
+                     }
+                ]                
+            }), 422
+        
+        user = User.querry.filter_by(email=email).first()
+             
+        if not user.email:
+             return jsonify({
+                'errors': [
+                    {'field': 'email',
+                     'message': 'invalid input, invalid email address'
+                     }
+                ]                
+            }), 422
+        
+
+        # password validation
+        if not password:
+                   return jsonify({
+                'errors': [
+                    {'field': 'password',
+                     'message': 'password field cannot be blank'
+                     }
+                ]                
+            }), 422
+        
+        if not password.isalphanumeric():
+             return jsonify({
+                'errors': [
+                    {'field': 'password',
+                     'message': 'password field must contain both numbers and characters'
+                     }
+                ]                
+            }), 422
+        
+        if user and bcrypt.checkpw(user.password, password):
+              access_token = create_access_token(identity=user.userId)
+              return jsonify({
+                'status': 'success',
+                'message': 'Login successful',
+                'data': {
+                    'accessToken': access_token,
+                    'user': {
+                        'userId': user.userId,
+                        'firstName': user.firstName,
+                        'lastName': user.lastName,
+                        'email': user.email,
+                        'phone': user.phone
+                   }
+                }
+            }), 201
+        
+        else:
+              return jsonify({
+          'status': 'Bad request',
+          'message': 'Authentication failed',
+          'statusCode': 401
+    }), 401
+        
