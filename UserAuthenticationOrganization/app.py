720e75d6420dbc6695da0ba6ae1df8d3d105299a
21Alul21 <augustine.alul@yahoo.com>
2024-07-08 23:46:02 +0100

    vercel app

--- UserAuthenticationOrganization/app.py
+++ UserAuthenticationOrganization/app.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =  "postgresql://austin_vom5_user:rsyDkZWwJzxOb2QnvquYt99vikIzcOyB@dpg-cq5fhoo8fa8c7386npm0-a/austin_vom5" #os.getenv('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(minutes=15)
 app.config['TESTING'] = True
